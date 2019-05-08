a2e0ad99916b7cf75143dc9062c057db7b65fbc9
Luca2411 <48845921+Luca2411@users.noreply.github.com>
2019-05-08 20:41:07 +0200

    Add files via upload
    Tests_functions_Luca

---
+++ test-OBV.R
@@ -0,0 +1,34 @@
+test_that("OBV works with simple input", {
+  p <- c( 20, 22, 24, 25, 23, 26, 28, 26, 29, 27, 28, 30, 27, 29, 28 )
+  expect_equal( OBV(p,11)[11], 70 )
+  expect_equal( OBV(p,10)[11], 70 )
+})
+
+test_that("First value is NA", {
+  p <- c( 20, 22, 24, 25, 23, 26, 28, 26, 29, 27, 28, 30, 27, 29, 28 )
+  n <- c(221, 241, 222, 283, 244, 265, 286, 267, 288, 279, 285, 304, 255, 234, 222)
+  
+  x <- all( is.na(OBV(p,n)[1]) )
+  expect_equal(x,TRUE)
+})
+
+test_that("input with a valid length", {
+  p <- c( 20, 22, 24, 25, 23, 26, 28, 26, 29, 27, 28, 30)
+  n <- c(221, 241, 222, 283, 244, 265, 286, 267, 288, 279, 285)
+  expect_error( OBV(p, n) )
+})
+
+test_that("xts input", {
+  expect_true(xts::is.xts(OBV(USGDP, USGDP)))
+  expect_equal(zoo::index(OBV(USGDP, USGDP)), zoo::index(USGDP))
+  
+  #expect_true(xts::is.xts(OBV(AAPL$AAPL.Close, AAPL$AAPL.Volume)))
+  #expect_equal(zoo::index(OBV(AAPL$AAPL.Close, AAPL$AAPL.Volume)), zoo::index(AAPL))
+})
+
+test_that("quantmod output as input", {
+  expect_length( OBV(BAC$Close,BAC$Volume), nrow(BAC) ) 
+  expect_length( OBV(BAC[,4],BAC$Volume), nrow(BAC) ) 
+})
+
+
